ac253ef977db362d785d6d17a0fba86d86e659d3
donkey103 <donkey103@naver.com>
2019-08-22 16:33:08 +0900

    테스트파일

---
+++ test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import random
+r = []
+b = []
+for i in range(50):
+    r.append([random.randint(40, 70), random.randint(140, 180), 1])
+    b.append([random.randint(60, 90), random.randint(160, 200), 0])
+
